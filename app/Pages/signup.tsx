8b75d630dacaaad03586ea59f367a73730c98f13
tiankay999 <tiankay999@gmail.com>
2025-10-22 22:58:20 +0000

    Signup,login, and reset password added

---
+++ app/Pages/signup.tsx
@@ -0,0 +1,58 @@
+import React, { FormEvent } from "react";
+
+export default function Signup() {
+
+
+
+    return (
+        <div>
+            <div className="relative min-h-screen  grid bg-black ">
+                <div className="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 ">
+                    <div
+                        className="relative sm:w-1/2 xl:w-3/5 bg-blue-500 h-full hidden md:flex flex-auto items-center justify-center p-10 overflow-hidden  text-white bg-no-repeat bg-cover relative"
+                        style={{ backgroundImage: "url('https://i.postimg.cc/2SnsYPyC/Chat-GPT-Image-Oct-22-2025-10-34-21-PM.png')" }}
+                    >
+                        <div className="absolute bg-black  opacity-25 inset-0 z-0"></div>
+                        <div className="w-full  lg:max-w-2xl md:max-w-md z-10 items-center text-center ">
+                            <div className=" font-bold leading-tight mb-6 mx-auto w-full content-center items-center "></div>
+                        </div>
+                    </div>
+
+                    <div className="md:flex md:items-center md:justify-left w-full sm:w-auto md:h-full xl:w-1/2 p-8  md:p-10 lg:p-14 sm:rounded-lg md:rounded-none ">
+                        <div className="max-w-xl w-full space-y-12">
+                            <div className="lg:text-left text-center">
+                                <div className="flex items-center justify-center ">
+                                    <div className="bg-black flex flex-col w-80 border border-gray-900 rounded-lg px-8 py-10">
+                                        <form className="flex flex-col space-y-8 mt-10" >
+                                            <label className="font-bold text-lg text-white ">Enter email</label>
+                                            <input
+                                                type="text"
+                                                placeholder="Email"
+                                                className="border rounded-lg py-3 px-3 mt-4 bg-black border-indigo-600 placeholder-white-500 text-white"
+                                            />
+                                            <label className="font-bold text-lg text-white">Password</label>
+                                            <input
+                                                type="password"
+                                                placeholder="****"
+                                                className="border rounded-lg py-3 px-3 bg-black border-indigo-600 placeholder-white-500 text-white"
+                                            />
+                                            <label className="font-bold text-lg text-white ">Enter full name </label>
+                                            <input
+                                                type="full name "
+                                                placeholder="full name "
+                                                className="border rounded-lg py-3 px-3 mt-4 bg-black border-indigo-600 placeholder-white-500 text-white"
+                                            />
+                                            <button type="submit" className="border border-indigo-600 bg-black text-white rounded-lg py-3 font-semibold">
+                                                Create Account
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
